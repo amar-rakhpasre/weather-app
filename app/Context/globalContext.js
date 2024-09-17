0b6b06f7bf97df73304904d85b8661bd5c0370e1
Amar Rakhpasre <rakhpasreamar@gmail.com>
2024-09-17 16:21:11 +0530

    fetching the data from api - initilise

--- app/Context/globalContext.js
+++ app/Context/globalContext.js
@@ -1,14 +1,34 @@
 "use Client";
-import React, { children, createContext, useContext, useState } from 'react'
+import axios from 'axios';
+import React, { children, createContext, useContext, useEffect, useState } from 'react'
 
 const GlobalContext = createContext();
 const GlobalContextUpdate = createContext();
 
 const GlobalContextProvider = ({ children }) => {
 
+  const [forecast, setForecast] = useState({});
+
+  const fetchForecast = async () => {
+   try {
+    const res = await axios.get("api/weather");
+
+    setForecast(res.data);
+
+   } catch (error) {
+    console.log("Error fetching forecast data: ", error.message);
+   } 
+  }
+
+
+  useEffect(() => {
+    fetchForecast();
+  }, []);
 
   return (
-    <GlobalContext.Provider value="Heooooool">
+    <GlobalContext.Provider value={{
+      forecast,
+    }}>
       <GlobalContextUpdate.Provider>
         {children}
       </GlobalContextUpdate.Provider>
--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -3,13 +3,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
     try {
-        const apiKey = process.env.OPENWEATHERMAP_API_KEY
+        const apiKey = process.env.OPENWEATHERMAP_API_KEY as string;
+
+        // Check if apiKey exists, otherwise throw an error
+        if (!apiKey) {
+            console.log("API key is missing.");
+        }
+
         const lat = 19.0364;  // Decimal format for latitude
         const lon = 72.8595;  // Decimal format for longitude
         const url =  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
         
         const res = await axios.get(url);
-
+        //    console.log(url);
         return NextResponse.json(res.data);
 
     } catch (error) {

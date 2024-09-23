7a936145fc85db5c327efa883d144c53a7f570ce
Amar Rakhpasre <rakhpasreamar@gmail.com>
2024-09-23 16:42:21 +0530

    complet: airPollution compaund

--- app/Components/AirPollution/AirPollution.tsx
+++ app/Components/AirPollution/AirPollution.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useGlobalContext } from '@/app/context/globalContext'
 import { thermo } from '@/app/utils/Icons';
+import { airQualityIndexText } from '@/app/utils/misc';
+import { Progress } from '@/components/ui/progress';
 import { Skeleton } from '@/components/ui/skeleton';
 import React from 'react'
 
@@ -15,13 +17,22 @@ function AirPollution() {
         return <Skeleton className='h-[12rem] w-full col-span-2 md:col-span-full'/>
     }
 
+    const airQualityIndex = airQuality.list[0].main.aqi * 10;
+
+    
+    const filterIndex = airQualityIndexText.find((item) => {
+        return item.rating === airQualityIndex;
+    });
+ 
+   
+
 
   return (
     <div className='air-pollution col-span-full sm-2:col-span-2 pt-6 px-4 h-[12rem] border rounded-lg flex flex-col gap-8 dark:bg-dark-grey shadow-sm dark:shadow-none'>
         <h2 className='flex items-center gap-2 font-medium'>
-            {thermo}Air Pollusion
-        </h2>
-        
+            {thermo}Air Pollusion</h2>
+            <Progress value={airQualityIndex} max={100} className='progress'/>
+            <p>Air Quality is {filterIndex?.Description} </p>
     </div>
   )
 }
--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -1,3 +1,28 @@
+import { Description } from "@radix-ui/react-dialog";
+
 export const kelvinToCelsius = (kelvin: number) =>{
     return Math.round(kelvin - 273.15);
 }
+
+export const airQualityIndexText = [
+    {
+        rating: 20,
+        Description: "good"
+    },
+    {
+        rating: 40,
+        Description: "fair"
+    },
+    {
+        rating: 60,
+        Description: "moderate"
+    },
+    {
+        rating: 80,
+        Description: "poor"
+    },
+    {
+        rating: 100,
+        Description: "very poor"
+    },
+]
